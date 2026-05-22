73fc8be509c81ced3bb56a369c78ad856c0d3504
Luis Majano <lmajano@gmail.com>
2026-05-22 19:01:49 +0200

    more tests

--- src/main/java/ortus/boxlang/jwt/util/KeyDictionary.java
+++ src/main/java/ortus/boxlang/jwt/util/KeyDictionary.java
@@ -20,36 +20,35 @@ import ortus.boxlang.runtime.scopes.Key;
  */
 public class KeyDictionary {
 
-	public static final Key	moduleName				= new Key( "bxjwt" );
-	public static final Key	JWTService				= new Key( "JWTService" );
-
 	public static final Key	algorithm				= new Key( "algorithm" );
-	public static final Key	keyAlgorithm			= new Key( "keyAlgorithm" );
-	public static final Key	encAlgorithm			= new Key( "encAlgorithm" );
-	public static final Key	keys					= new Key( "keys" );
-	public static final Key	secret					= new Key( "secret" );
-	public static final Key	privateKey				= new Key( "privateKey" );
-	public static final Key	publicKey				= new Key( "publicKey" );
-	public static final Key	jwk						= new Key( "jwk" );
-	public static final Key	payload					= new Key( "payload" );
-	public static final Key	token					= new Key( "token" );
-	public static final Key	key						= new Key( "key" );
-	public static final Key	headers					= new Key( "headers" );
 	public static final Key	claims					= new Key( "claims" );
-	public static final Key	options					= new Key( "options" );
-	public static final Key	generateIat				= new Key( "generateIat" );
-	public static final Key	generateJti				= new Key( "generateJti" );
 	public static final Key	clockSkew				= new Key( "clockSkew" );
-	public static final Key	validateClaims			= new Key( "validateClaims" );
-
+	public static final Key	defaultAlgorithm		= new Key( "defaultAlgorithm" );
+	public static final Key	defaultDecryptionKey	= new Key( "defaultDecryptionKey" );
+	public static final Key	defaultEncAlgorithm		= new Key( "defaultEncAlgorithm" );
+	public static final Key	defaultEncryptionKey	= new Key( "defaultEncryptionKey" );
+	public static final Key	defaultKeyAlgorithm		= new Key( "defaultKeyAlgorithm" );
 	public static final Key	defaultSigningKey		= new Key( "defaultSigningKey" );
 	public static final Key	defaultVerifyKey		= new Key( "defaultVerifyKey" );
-	public static final Key	defaultEncryptionKey	= new Key( "defaultEncryptionKey" );
-	public static final Key	defaultDecryptionKey	= new Key( "defaultDecryptionKey" );
-	public static final Key	defaultAlgorithm		= new Key( "defaultAlgorithm" );
-	public static final Key	defaultKeyAlgorithm		= new Key( "defaultKeyAlgorithm" );
-	public static final Key	defaultEncAlgorithm		= new Key( "defaultEncAlgorithm" );
-
+	public static final Key	encAlgorithm			= new Key( "encAlgorithm" );
+	public static final Key	generateIat				= new Key( "generateIat" );
+	public static final Key	generateJti				= new Key( "generateJti" );
+	public static final Key	headers					= new Key( "headers" );
+	public static final Key	iat						= new Key( "iat" );
 	public static final Key	isValidLicense			= new Key( "isValidLicense" );
+	public static final Key	jwk						= new Key( "jwk" );
+	public static final Key	jti						= new Key( "jti" );
+	public static final Key	JWTService				= new Key( "JWTService" );
+	public static final Key	key						= new Key( "key" );
+	public static final Key	keyAlgorithm			= new Key( "keyAlgorithm" );
+	public static final Key	keys					= new Key( "keys" );
+	public static final Key	moduleName				= new Key( "bxjwt" );
+	public static final Key	options					= new Key( "options" );
+	public static final Key	payload					= new Key( "payload" );
+	public static final Key	privateKey				= new Key( "privateKey" );
+	public static final Key	publicKey				= new Key( "publicKey" );
+	public static final Key	secret					= new Key( "secret" );
+	public static final Key	token					= new Key( "token" );
+	public static final Key	validateClaims			= new Key( "validateClaims" );
 
 }
